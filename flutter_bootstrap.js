bae2c38da846c6d059f6fe4f4e4f944b41909baf
niwradCodera <niwrad.pro@gmail.com>
2025-05-18 02:48:54 +0100

    changement uri backend

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "721272627"
+    serviceWorkerVersion: "1540519278"
   }
 });
